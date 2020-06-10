e249ee9f50ada92e891feb2dd2b83b30d9439be0
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-06-10 13:08:18 +0100

    gatsby-source-filesystem
    npm install --save gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
     `gatsby-transformer-sharp`,
