f452c57bdf59a972fc7d68cf050e2ba9d7a0922e
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-06-14 16:59:39 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,22 +13,27 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 
   const postTemplate = path.resolve("src/templates/post.js")
 
-  return graphql(`{
-      allMarkdownRemark(){
-          edges {
-              node{
-                  html
-                  id
-                  frontmatter{
-                      path
-                      title
-                  }
-              }
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+            }
           }
+        }
       }
-  }`).then(res => {
+    }
+  `).then(res => {
     if (res.errors) {
       return Promise.reject(res.eroors)
     }
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({ path: node.frontmatter.path, component: postTemplate })
+    })
   })
 }
